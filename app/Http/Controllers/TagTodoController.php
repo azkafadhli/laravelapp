499252c9189f098e8fc5eff4b155593d697bc743
azkafr92 <azkafr92@gmail.com>
2021-11-27 00:06:01 +0700

    TagTodo crud

--- app/Http/Controllers/TagTodoController.php
+++ app/Http/Controllers/TagTodoController.php
@@ -5,16 +5,14 @@ namespace App\Http\Controllers;
 use App\TagTodo;
 use Illuminate\Http\Request;
 
-class TagTodoController extends Controller
-{
+class TagTodoController extends Controller {
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        //
+    public function index() {
+        return TagTodo::all();
     }
 
     /**
@@ -23,9 +21,11 @@ class TagTodoController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(Request $request) {
+        $request->validate(['tag_id' => ['required'], 'todo_id' => ['required']]);
+        $tag_todo = TagTodo::create($request->only(['tag_id', 'todo_id']));
+        $tag_todo->save();
+        return $tag_todo;
     }
 
     /**
@@ -34,9 +34,10 @@ class TagTodoController extends Controller
      * @param  \App\TagTodo  $TagTodo
      * @return \Illuminate\Http\Response
      */
-    public function show(TagTodo $TagTodo)
-    {
-        //
+    public function show(int $id) {
+        $tag_todo = TagTodo::with(['tag', 'todo', 'account'])
+            ->where('id', '=', $id)->get();
+        return $tag_todo;
     }
 
     /**
@@ -46,9 +47,10 @@ class TagTodoController extends Controller
      * @param  \App\TagTodo  $TagTodo
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, TagTodo $TagTodo)
-    {
-        //
+    public function update(Request $request, int $id) {
+        $tag_todo = TagTodo::find($id);
+        $tag_todo->update($request->only(['tag_id', 'todo_id']));
+        return $tag_todo;
     }
 
     /**
@@ -57,8 +59,7 @@ class TagTodoController extends Controller
      * @param  \App\TagTodo  $TagTodo
      * @return \Illuminate\Http\Response
      */
-    public function destroy(TagTodo $TagTodo)
-    {
-        //
+    public function destroy(int $id) {
+        return TagTodo::destroy($id);
     }
 }
--- app/TagTodo.php
+++ app/TagTodo.php
@@ -2,9 +2,15 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class TagTodo extends Model {
-    protected $table = 'tag_todo';
+class TagTodo extends Pivot {
     protected $fillable = ['todo_id', 'tag_id'];
+
+    public function todo() {
+        return $this->belongsTo('App\Todo', 'todo_id', 'id');
+    }
+    public function tag() {
+        return $this->belongsTo('App\Tag', 'tag_id', 'id');
+    }
 }
